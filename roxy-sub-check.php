6e387ae52afd30affea78c88dd633d8327cc1a20
Newport Roxy (AI Team) <info@newportroxy.com>
2026-04-01 14:03:00 -0700

    Update roxy-sub-check.php

--- roxy-sub-check.php
+++ roxy-sub-check.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Roxy Subscription Check
  * Description: NFC-friendly membership verification page for WooCommerce Subscriptions. Per-subscription photo, scan log, and customer photo upload.
- * Version: 1.3.4
+ * Version: 1.3.5
  * Author: Newport Roxy (AI Team)
  * Update URI: https://github.com/Tototex/roxy-sub-check
  */
@@ -13,7 +13,7 @@ require_once __DIR__ . '/includes/class-roxy-sub-updater.php';
 
 \RoxySub\Updater::init([
   'plugin_file' => plugin_basename(__FILE__),
-  'version'     => '1.3.4',
+  'version'     => '1.3.5',
   'github_repo' => 'Tototex/roxy-sub-check',
   'slug'        => 'roxy-sub-check',
 ]);
@@ -81,12 +81,19 @@ class Roxy_Sub_Check {
     return $vars;
   }
 
+  private static function can_access_member_check(): bool {
+    return current_user_can('manage_woocommerce') || current_user_can('edit_posts');
+  }
+
   public static function template_redirect() {
     if (get_query_var('roxy_member_check') == 1) {
-      if (!current_user_can('manage_woocommerce')) {
+      if (!is_user_logged_in()) {
         $redirect_back = home_url('/member-check/') . (isset($_GET['sub']) ? '?sub=' . absint($_GET['sub']) : '');
         wp_redirect(wp_login_url($redirect_back));
         exit;
+      }
+      if (!self::can_access_member_check()) {
+        wp_die('You do not have permission to access Member Check.');
       }
       self::render_page();
       exit;
